55618399ac281036d3c0485326da321e39351a9f
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-01-11 20:07:47 -0300

    feat: adding homepage style

---
+++ frontend/src/utils/images.utils.js
@@ -0,0 +1,12 @@
+import money from '../assets/change_money.png';
+import { FcCurrencyExchange } from 'react-icons/fc';
+import { RiMoneyDollarCircleFill } from 'react-icons/ri';
+import { FaSearchDollar } from 'react-icons/fa';
+
+const applicationImages = {
+    changeMoney: <FcCurrencyExchange size={60} />,
+    money: <RiMoneyDollarCircleFill size={40} />,
+    searchMoney: <FaSearchDollar size={30}/>
+}
+
+export default applicationImages;
