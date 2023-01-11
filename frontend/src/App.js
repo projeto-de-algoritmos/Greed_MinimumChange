e626cfdbe2f1020b9dc238d5e97e5efff26b426f
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-01-11 19:03:17 -0300

    feat: initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Home from "./pages/Home/Home";
+import './global/global.css';
+
+function App() {
+  return (
+    <div className="main-container">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const Home = () => {
+    return (
+        <div className="home-container centraliza">
+            <div className="home-content">
+            </div>
+        </div>
+    );
+};
+
+export default Home;
