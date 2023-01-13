7055fe7c541f90efd84065b6a79b9881e6ed80fa
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-01-12 21:40:20 -0300

    chore: adding files and libs

--- frontend/src/utils/images.utils.js
+++ frontend/src/utils/images.utils.js
@@ -2,11 +2,13 @@ import money from '../assets/change_money.png';
 import { FcCurrencyExchange } from 'react-icons/fc';
 import { RiMoneyDollarCircleFill } from 'react-icons/ri';
 import { FaSearchDollar } from 'react-icons/fa';
+import { AiFillWarning } from "react-icons/ai";
 
 const applicationImages = {
     changeMoney: <FcCurrencyExchange size={60} />,
     money: <RiMoneyDollarCircleFill size={40} />,
-    searchMoney: <FaSearchDollar size={30}/>
+    searchMoney: <FaSearchDollar size={30} />,
+    warning: <AiFillWarning size={30} color="red" /> 
 }
 
 export default applicationImages;
